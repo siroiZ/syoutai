8bbf15d99298e36e2649a01a3979ff7e9f51e6e2
BlackTigerKnight <144640335+BlackTigerKnight@users.noreply.github.com>
2024-04-03 16:01:10 +0900

    timer UP
    タイム30秒以下で赤になるよう追加
    まだ0分0秒0ミリ秒になると
    マイナスになるバグ有。

--- Src/Timer/Timer.cpp
+++ Src/Timer/Timer.cpp
@@ -3,23 +3,33 @@
 #include "time.h"
 
 void CntTimer::Init() {		// ‰Šú‰»
-	cnttimeM_m = 0;		// •ª
-	cnttimeS_m = 0;		// •b
+	cnttimeM_m = 1;		// •ª
+	cnttimeS_m = 30;		// •b
 	cnttimeMS_m = 0;	// ƒ~ƒŠ•b
 }
 
 void CntTimer::Step() {		// ’Êí
-	cnttimeMS_m++;
-	if (cnttimeMS_m >= 10) {
-		cnttimeMS_m = 0;
-		cnttimeS_m++;
+
+	if (cnttimeM_m >= 0 && cnttimeS_m >= 0 && cnttimeMS_m >= 0) {
+		cnttimeMS_m--;
+		if (cnttimeMS_m < 0) {
+			cnttimeMS_m = 10;
+			cnttimeS_m--;
+		}
+		if (cnttimeS_m < 0) {
+			cnttimeS_m = 60;
+			cnttimeM_m--;
+		}
 	}
-	if (cnttimeS_m >= 60) {
-		cnttimeS_m = 0;
-		cnttimeM_m++;
-	}
+
 }
 
 void CntTimer::Draw() {		// •`‰æ
-	DrawFormatString(0, 0, GetColor(255, 255, 255), "%0d:%0d:%0d", cnttimeM_m, cnttimeS_m, cnttimeMS_m);
+	if (cnttimeM_m == 0 && cnttimeS_m <= 30 && cnttimeMS_m >= 0) {
+		DrawFormatString(0, 0, GetColor(255, 100, 0), "%0d:%0d:%0d", cnttimeM_m, cnttimeS_m, cnttimeMS_m);
+
+	}
+	else
+		DrawFormatString(0, 0, GetColor(255, 255, 255), "%0d:%0d:%0d", cnttimeM_m, cnttimeS_m, cnttimeMS_m);
+
 }
