3615a15ac7b84ef74b32792b509ff8941c215609
siroiZ <144640248+siroiZ@users.noreply.github.com>
2024-04-07 16:59:19 +0900

    a

--- Src/SceneClear/SceneClear.cpp
+++ Src/SceneClear/SceneClear.cpp
@@ -2,22 +2,39 @@
 #include "../ScenePlay/Scene.h"
 #include "SceneClear.h"
 
+#define CLEAR_PATH "Data/Title/Clear.png"
+#define CLEAR_HAIKEI_PATH "Data/Haikei/画像1.png"
+
+int ClearHandle;
+int ClearHikeiHandle;
+
 void InitClear()
 {
-
+	ClearHandle = LoadGraph(CLEAR_PATH);
+	ClearHikeiHandle = LoadGraph(CLEAR_HAIKEI_PATH);
+	g_CurrentSceneID = SCENE_ID_LOOP_CLEAR;
 }
 
 void StepClear()
 {
-
+	//Enterキー押されたなら
+	if (CheckHitKey(KEY_INPUT_RETURN) == 1)
+	{
+		g_CurrentSceneID = SCENE_ID_FIN_CLEAR;
+	}
 }
 
 void DrawClear()
 {
-
+	//背景
+	DrawGraph(-10, -10, ClearHikeiHandle, true);
+	//ゲームオーバー描画
+	DrawGraph(150, 100, ClearHandle, true);
+	DrawFormatString(230, 300, GetColor(0, 200, 0), "タイトルへ戻る：Enter");
 }
 
 void FinClear()
 {
-
+	//タイトルシーンへ移動
+	g_CurrentSceneID = SCENE_ID_INIT_TITLE;
 }
--- Src/SceneGameOver/SceneGameOver.cpp
+++ Src/SceneGameOver/SceneGameOver.cpp
@@ -2,23 +2,39 @@
 #include "../ScenePlay/Scene.h"
 #include "SceneGameOver.h"
 
+#define GAMEOVER_PATH "Data/Title/Gameover.png"
+#define GAMEOVER_HAIKEI_PATH "Data/Haikei/画像1.png"
+
+int GameOverHandle;
+int GameOverHikeiHandle;
 
 void InitGameOver()
 {
-
+	GameOverHandle = LoadGraph(GAMEOVER_PATH);
+	GameOverHikeiHandle = LoadGraph(GAMEOVER_HAIKEI_PATH);
+	g_CurrentSceneID = SCENE_ID_LOOP_GAMEOVER;
 }
 
 void StepGameOver()
 {
-
+	//Enterキー押されたなら
+	if (CheckHitKey(KEY_INPUT_RETURN) == 1)
+	{
+		g_CurrentSceneID = SCENE_ID_FIN_GAMEOVER;
+	}
 }
 
 void DrawGameOver()
 {
-
+	//背景
+	DrawGraph(-10, -10, GameOverHikeiHandle, true);
+	//ゲームオーバー描画
+	DrawGraph(55, 100, GameOverHandle, true);
+	DrawFormatString(250, 300, GetColor(0, 200, 0), "やり直す：Enter");
 }
 
 void FinGameOver()
 {
-
+	//プレイシーンへ移動
+		g_CurrentSceneID = SCENE_ID_INIT_PLAY;
 }
